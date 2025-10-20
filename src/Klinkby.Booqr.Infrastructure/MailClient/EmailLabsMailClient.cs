ad68835229140ed9c068eeefe41eebf725c21ed7
Mads Klinkby <1681112+klinkby@users.noreply.github.com>
2025-10-20 13:36:50 +0200

    fix: mailer err handling

--- src/Klinkby.Booqr.Infrastructure/MailClient/EmailLabsMailClient.cs
+++ src/Klinkby.Booqr.Infrastructure/MailClient/EmailLabsMailClient.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,30 +29,40 @@ internal sealed partial class EmailLabsMailClient(
                 new("text", message.Body),
                 new("from", _fromAddress)
             }.AsReadOnly());
-        // https://dev.emaillabs.io/#api-Send-new_sendmail
-        HttpResponseMessage responseMessage = await httpClient.PostAsync(
-            new Uri("api/new_sendmail", UriKind.Relative),
-            form,
-            cancellationToken);
-        if (responseMessage.IsSuccessStatusCode)
-        {
-            _log.SendEmailSuccess();
-            return;
-        }
-
-        EmailLabsMailClientResponse response =
-            await responseMessage.Content.ReadFromJsonAsync(
-                MailJsonSerializerContext.Default.EmailLabsMailClientResponse,
-                cancellationToken)
-            ?? new EmailLabsMailClientResponse((int)responseMessage.StatusCode, "Failed", "General error sending mail");
+        var response = EmailLabsMailClientResponse.FromGeneralNetworkError();
         try
         {
-            responseMessage.EnsureSuccessStatusCode();
+            // https://dev.emaillabs.io/#api-Send-new_sendmail
+            HttpResponseMessage responseMessage = await httpClient.PostAsync(
+                new Uri("api/new_sendmail", UriKind.Relative),
+                form,
+                cancellationToken); // throws if there's no connection
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                _log.SendEmailSuccess();
+                return;
+            }
+
+            response = await DeserializeFailureResponse(responseMessage, cancellationToken);
+
+            responseMessage.EnsureSuccessStatusCode(); // throws HttpRequestException
         }
         catch (HttpRequestException ex)
         {
             throw new MailClientException(response.Message, response.Status, response.Code, ex);
         }
+    }
+
+    async private static Task<EmailLabsMailClientResponse> DeserializeFailureResponse(HttpResponseMessage responseMessage,
+        CancellationToken cancellationToken)
+    {
+        EmailLabsMailClientResponse response;
+        response = await responseMessage.Content.ReadFromJsonAsync(
+                       MailJsonSerializerContext.Default.EmailLabsMailClientResponse,
+                       cancellationToken)
+                   ?? EmailLabsMailClientResponse.FromGeneralServerError(responseMessage.StatusCode);
+        return response;
     }
 
     private sealed partial class LoggerMessages(ILogger<EmailLabsMailClient> logger)
@@ -66,10 +77,18 @@ internal sealed partial class EmailLabsMailClient(
     }
 
     private sealed record EmailLabsMailClientResponse(
-        [property: JsonPropertyName("code")]    int Code,
-        [property: JsonPropertyName("status")]  string Status,
-        [property: JsonPropertyName("message")] string Message,
-        [property: JsonPropertyName("data")]    object? Data = null);
+        [property: JsonPropertyName("code")] int Code,
+        [property: JsonPropertyName("status")] string Status,
+        [property: JsonPropertyName("message")]
+        string Message,
+        [property: JsonPropertyName("data")] object? Data = null)
+    {
+        public static EmailLabsMailClientResponse FromGeneralNetworkError() =>
+            new(0, "NetworkError", "General network error");
+
+        public static EmailLabsMailClientResponse FromGeneralServerError(HttpStatusCode statusCode) =>
+            new((int)statusCode, "ServerError", $"General server error: {statusCode}");
+    }
 
     [JsonSerializable(typeof(EmailLabsMailClientResponse))]
     private sealed partial class MailJsonSerializerContext : JsonSerializerContext;
