9887b3618dba841488689f63517a0c20e76cbbc8
Mads Klinkby <1681112+klinkby@users.noreply.github.com>
2025-10-22 22:35:30 +0200

    test: Handlebars

---
+++ tests/Klinkby.Booqr.Application.Tests/HandlebarsTests.cs
@@ -0,0 +1,71 @@
+﻿namespace Klinkby.Booqr.Application.Tests;
+
+public sealed class ReplaceHandlebarsTests
+{
+    [Theory]
+    [AutoData]
+    public void GIVEN_TemplateWithSingleToken_THEN_TokenIsReplaced([Frozen] string value)
+    {
+        var actual = Handlebars.Replace("Hello {{name}}",
+            new Dictionary<string, string> { ["name"] = value });
+
+        Assert.Equal($"Hello {value}", actual);
+    }
+
+    [Theory]
+    [AutoData]
+    public void GIVEN_TemplateWithMultipleTokens_THEN_AllTokensAreReplaced(string value1,
+        string value2)
+    {
+        var actual = Handlebars.Replace("Start {{first}}-mid-{{second}}-end",
+            new Dictionary<string, string>
+            {
+                ["first"] = value1,
+                ["second"] = value2
+            });
+
+        Assert.Equal($"Start {value1}-mid-{value2}-end", actual);
+    }
+
+    [Theory]
+    [AutoData]
+    public void GIVEN_TemplateWithRepeatedToken_THEN_AllInstancesAreReplaced(string value)
+    {
+        var actual = Handlebars.Replace("{{value}},{{value}},{{value}}",
+            new Dictionary<string, string> { ["value"] = value });
+
+        Assert.Equal($"{value},{value},{value}", actual);
+    }
+
+    [Theory]
+    [AutoData]
+    public void GIVEN_TemplateWithToken_HAVING_MissingReplacement_THEN_ThrowsArgumentException(string value)
+    {
+        Assert.Throws<ArgumentException>(() =>
+            Handlebars.Replace("X-{{present}}-Y-{{missing}}-Z",
+                new Dictionary<string, string> { ["present"] = value }));
+    }
+
+
+    [Fact]
+    public void GIVEN_EmptyTemplate_THEN_ReturnsEmptyString()
+    {
+        var actual = Handlebars.Replace(ReadOnlySpan<char>.Empty, new Dictionary<string, string>());
+
+        Assert.Equal(string.Empty, actual);
+    }
+
+    [Theory]
+    [AutoData]
+    public void GIVEN_TokensAtEdgesAndAdjacent_THEN_AllPositionsHandledCorrectly(string a, string b)
+    {
+        var actual = Handlebars.Replace("{{a}}--{{b}}{{a}}",
+            new Dictionary<string, string>
+            {
+                ["a"] = a,
+                ["b"] = b
+            });
+
+        Assert.Equal($"{a}--{b}{a}", actual);
+    }
+}
